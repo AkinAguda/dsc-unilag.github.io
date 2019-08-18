60712dd672282b54252d987b634d90826c99c6c5
Akinwunmi Aguda <39712415+AkinAguda@users.noreply.github.com>
2019-08-18 18:28:15 +0100

    Delete firebase.js

--- firebase.js
+++
@@ -1,7 +0,0 @@
-firebase.initializeApp({
-  apiKey: "AIzaSyAxKjLcn5dYAIU-9clM5UaQ-h5xevNs4Hs",
-  authDomain: "dsc-unilag.firebaseapp.com",
-  projectId: "dsc-unilag"
-});
-
-// var db = firebase.firestore();
